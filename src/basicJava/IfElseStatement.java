2936341dd8271e75b56e45fd1d5d527979a64384
ashraf <mdashrafkhanqa@gmail.com>
2019-02-23 10:45:41 -0500

    IfElseStatement new class

---
+++ src/basicJava/IfElseStatement.java
@@ -0,0 +1,27 @@
+package basicJava;
+
+public class IfElseStatement {
+
+    public static void main(String[] args) {
+
+        int number = 20;
+        int test = 100;
+        int banana = 10;
+
+
+        String Result="Result";
+        String Result2="banana";
+        String Result3="Result test";
+
+        if (Result==Result2|| Result!=Result2) {
+
+            System.out.println(test);
+
+        } else {
+
+            System.out.println(banana);
+        }
+
+
+    }
+}
