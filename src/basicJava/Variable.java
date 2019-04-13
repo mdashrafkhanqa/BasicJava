bf56fd4d6a5bbfec9a5d2ed8b18d64fa79a927bc
ashraf <mdashrafkhanqa@gmail.com>
2019-04-13 18:14:00 -0400

    TryCatch class created

--- src/basicJava/Variable.java
+++ src/basicJava/Variable.java
@@ -3,12 +3,18 @@ package basicJava;
 public class Variable {
 
     static int Banana = 20;
+    static int Apple = 30;
+    static int Mango = 40;
+
 
 
     public static void main(String[] args) {
 
 
+        System.out.println(Apple);
         System.out.println(Banana);
+        System.out.println(Mango);
+        System.out.println(Apple+Banana+Mango);
 
     }
 }
