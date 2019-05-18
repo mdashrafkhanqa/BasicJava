f30108b2ed621f4ed3f2de197de8658ccf92b27f
ashraf <mdashrafkhanqa@gmail.com>
2019-05-18 10:08:34 -0400

    Nested IfElseStatement Created

---
+++ src/basicJava/NestedIfElseStatement.java
@@ -0,0 +1,29 @@
+package basicJava;
+
+public class NestedIfElseStatement {
+
+
+    public static void main(String[] args) {
+
+        int Apple=20;
+
+        if(Apple==20){
+        if(Apple<30)
+
+            System.out.println("Apple is less then 30");
+
+        if(Apple<25)
+
+            System.out.println("Apple is less then 25");
+
+        else
+
+            System.out.println("Apple is greater then 30");
+        }
+
+
+        }
+
+
+    }
+
